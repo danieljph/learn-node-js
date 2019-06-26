9c2ada9d4debda2f7fd945ed5189537221618567
Daniel Joi Partogi Hutapea <daniel.joi.ph@gmail.com>
2019-06-26 16:57:40 +0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('view engine', 'pug')
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
---
+++ utils/commons.js
@@ -0,0 +1,11 @@
+module.exports = {
+    add(operand1, operand2)
+    {
+        return operand1+operand2;
+    },
+
+    multiply(operand1, operand2)
+    {
+        return operand1*operand2;
+    }
+};
